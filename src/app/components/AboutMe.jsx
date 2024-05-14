4ddc643586c22ca69f93ac5a7d8cd3310f0e9315
Julz102296 <sagadaljulios35@gmail.com>
2024-05-15 02:53:03 +0800

    compressed cv

--- src/app/components/AboutMe.jsx
+++ src/app/components/AboutMe.jsx
@@ -101,7 +101,7 @@ export default function AboutMe() {
 
                   <a
                     className="group relative inline-flex items-center overflow-hidden px-8 py-3 text-slate-600 focus:outline-none focus:ring active:text-slate-400"
-                    href="/pdf/Julios_Sagadal-CV.pdf"
+                    href="/pdf/Julio_SagadalCV_2024_compressed.pdf"
                     download
                   >
                     <span className="absolute -end-full transition-all group-hover:end-4">
