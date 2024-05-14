dc2dfba3f3446c11840e062a1e580bbeac96790b
Julz102296 <sagadaljulios35@gmail.com>
2024-05-14 20:12:49 +0800

    scroll bar bug fixed

--- src/app/customs/porjects/Portfoliooo.jsx
+++ src/app/customs/porjects/Portfoliooo.jsx
@@ -4,7 +4,7 @@ import styles from './portfoliooo.module.css';
 function Portfoliooo() {
   return (
     <div className={`${styles.container} flex justify-center items-center h-screen overflow-hidden`}>
-      <iframe src="/pdf/portfolio-2.pdf" className={`${styles.pdfEmbed} ${styles.mobileResponsive}`} title="Embedded PDF"></iframe>
+      <iframe src="/pdf/portfolio_v22.pdf" className={`${styles.pdfEmbed} ${styles.mobileResponsive}`} title="Embedded PDF"></iframe>
     </div>
   );
 }
